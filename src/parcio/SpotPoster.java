3379ae222ea12a5ef09dbb5a0f108fda84ac1e22
Aaron Vontell <vontell@mit.edu>
2015-02-01 01:11:28 -0500

    Changed Spot Poster

--- src/parcio/SpotPoster.java
+++ src/parcio/SpotPoster.java
@@ -15,7 +15,7 @@ import resources.Constructors;
 public class SpotPoster {
 
 	private String apiKey;
-	private String BASE_URL = "https://parcio.herokuapp.com/parkspot/";
+	private String BASE_URL = "https://parcio.herokuapp.com/parkspot";
 	private String data;
 	private String finalURL;
 	
@@ -32,7 +32,7 @@ public class SpotPoster {
 		apiKey = key;
 		
 		//Should eventually move to loading
-		finalURL = "https://parcio.herokuapp.com/parkspot";
+		finalURL = BASE_URL;
 		
 	}
 	
@@ -66,7 +66,6 @@ public class SpotPoster {
 		jObj.put("per_time", per_time);
 		jObj.put("free_time", free_time);
 		jObj.put("available", available);
-		jObj.put("time_create", System.currentTimeMillis());
 		
 		System.out.println(jObj.toString());
 		
